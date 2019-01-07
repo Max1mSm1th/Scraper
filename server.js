13aee6d6374b78b81f8c136d6854ab7bf57dc6a9
Max Smith <maxsmith216@gmail.com>
2019-01-06 22:54:05 -0800

    updated app

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ var app = express();
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+  app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+  app.set("view engine", "handlebars");
 
 var router = express.Router();
 
